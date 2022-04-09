627204ecf2215a3736cbcab120f61c3e0e476d9b
Terra Hanks <vampyroteuthisinfernalis@protonmail.com>
2022-04-09 11:06:00 -0800

    refactored to remove Math.abs

--- SumOfPositive.js
+++ SumOfPositive.js
@@ -5,7 +5,14 @@
 
 // Note: if there is nothing to sum, the sum is default to 0.
 
-function positiveSum(array) {
-    let newArr = array.filter(num => num > 0).reduce((acc,c) => acc + c, 0)
-    return (Math.abs(newArr))
-    }
+//2nd attempt refctored out the Math.abs
+function positiveSum(arr) {
+  let posNum = arr.filter(num => num >0).reduce((a, b) => a+b, 0)
+    return posNum
+}
+
+//1st attempt
+// function positiveSum(array) {
+//     let newArr = array.filter(num => num > 0).reduce((acc,c) => acc + c, 0)
+//     return (Math.abs(newArr))
+//     }
