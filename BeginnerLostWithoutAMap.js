88bc71dafd09165c50dfe6078dac432573c73373
Terra Hanks <vampyroteuthisinfernalis@protonmail.com>
2022-04-09 12:55:35 -0800

    updated to correct folder

---
+++ BeginnerLostWithoutAMap.js
@@ -0,0 +1,16 @@
+// Description:
+// Given an array of integers, return a new array with each value doubled.
+// For example:
+
+// `[1, 2, 3] --> [2, 4, 6]`
+
+
+
+
+function maps(x){
+    var newArr = [];
+    x.map(function(y){
+newArr.push(y*2);
+});
+    return newArr;
+}
