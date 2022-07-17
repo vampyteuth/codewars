d6e85b8d0a37a4fc0a24268f575e77a091d550f1
Terra Hanks <vampyroteuthisinfernalis@protonmail.com>
2022-07-17 12:28:42 -0600

    Created ThirdAngleOFATriangle.js

---
+++ ThirdAngleOfATriangle.js
@@ -0,0 +1,9 @@
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+const otherAngle = (a, b) => {
+    return c = 180-(a+b)
+   }
