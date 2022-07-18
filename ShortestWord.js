e7ed20e763cec0273b47e138c3f241d11611ad4f
Terra Hanks <vampyroteuthisinfernalis@protonmail.com>
2022-07-17 18:18:37 -0600

    Created ShortestWord.js

---
+++ ShortestWord.js
@@ -0,0 +1,8 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+const findShort = s => {
+    const arr = s.split(' ').sort((a, b) => a.length - b.length)
+    return arr[0].length
+}
